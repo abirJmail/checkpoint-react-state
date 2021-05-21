f41411e9d59dcb7bfe74308f82179b4af7ef25b9
abirJmail <jmailabir@gmail.com>
2021-05-21 11:48:45 +0100

    reactState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  
+    state = { show : true, };
+    toggleDiv = () => {
+        const { show } = this.state;
+        this.setState( { show : !show } )
+    }
+    
+    render() {
+        return (
+          <div className="App">
+                <button onClick={ this.toggleDiv }>Toggle div</button>
+                <br /><br />
+                { this.state.show && <Box /> }
+            </div>
+        );
+    }
+}
+
+class Box extends Component{
+  
+  state = {
+    person: {
+      fullName : "Abir Jmail",
+      bio : "Developed full-stack web applications which processed, analyzed, and rendered data visually.",
+      imgSrc : "https://avatars.githubusercontent.com/u/82514715?v=4",
+      profession : "Full stack developer",
+    },
+    arr: [],
+  };
+    render(){
+        return(
+            <div>
+              { this.state.person.fullName} <br /><br />
+              { this.state.person.bio} <br /><br />
+              { this.state.person.profession}
+              </div>
+        )
+    }
 }
 
 export default App;
